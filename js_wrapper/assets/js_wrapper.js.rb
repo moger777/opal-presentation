03e334e2ee1eb529069cd9e8fabccf82762b19b2
Thijs de Vries <tdevries@patientslikeme.com>
2014-03-13 21:10:08 -0400

    Example of simple wrapper that can call javascript methods as if they were opal.

--- js_wrapper/assets/js_wrapper.js.rb
+++ js_wrapper/assets/js_wrapper.js.rb
@@ -1,3 +1,20 @@
 class JSWrapper
+  def initialize(js_object)
+    `
+      (function(){
+        var names = [];
+        for (var key in js_object)  {
+          if(typeof js_object[key] === 'function') {
+            names.push(key);
+          }
+        }
+        return names;
+      })()
+    `.each do |method_name|
+      define_singleton_method method_name do
+        `js_object[method_name].call(js_object)`
+      end
+    end
+  end
 end
 
--- js_wrapper/spec/js_wrapper_spec.rb
+++ js_wrapper/spec/js_wrapper_spec.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 describe JSWrapper do
-  it "should exist" do
-    JSWrapper.should_not be_nil
+  it "should be able to call javascript methods" do
+    method_called = false
+
+    js_object = %x| {
+        call_method: function() {
+          method_called = true;
+        }
+      }
+    |
+
+    wrapper = JSWrapper.new(js_object)
+    wrapper.call_method
+    method_called.should == true
+    expect(method_called).to be(true)
   end
 end
+
