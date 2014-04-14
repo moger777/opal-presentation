92a1147905dce63bd976430b716e8f022ee82dd9
Thijs de Vries <tdevries@patientslikeme.com>
2014-04-14 18:45:25 -0400

    use alias_native instead of expose.

--- opal-jquery/assets/datepicker.js.rb
+++ opal-jquery/assets/datepicker.js.rb
@@ -4,7 +4,8 @@ require 'jquery-ui-datepicker'
 require 'opal-jquery'
 
 Element.module_eval do
-  expose :datepicker
+  # can also be used as alias_native :someThingCamelCased, :some_thing_underscored
+  alias_native :datepicker
 end
 
 Document.ready? do
--- opal-jquery/config.ru
+++ opal-jquery/config.ru
@@ -4,9 +4,6 @@ Bundler.require
 use Rack::Static
 
 run Opal::Server.new { |s|
-  # the entry-point (main) to the app
-  s.main = 'application'
-
   # add additional dirs to opal/sprockets load path
   s.append_path 'assets'
 }
