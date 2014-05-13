92cf232a6889d4e8201f01f9e5312a512b261c09
Thijs de Vries <tdevries@patientslikeme.com>
2014-05-13 17:59:48 -0400

    lets simplify this.

--- bridge_class_example/assets/application.js.rb
+++ bridge_class_example/assets/application.js.rb
@@ -7,16 +7,11 @@ var SomeFunction = function(){};
 SomeFunction.prototype.someMethod = function(){ return "some_string" };
 SomeFunction.prototype.anotherMethod = function(){ return "another_string" };
 
-var AnotherFunction = function(){};
-AnotherFunction.prototype = new SomeFunction();
 `
 
 class InheritsFromSomeFunction < `SomeFunction`
   alias_native :some_method, :someMethod
   alias_native :another_method, :anotherMethod
-end
-
-class What < `AnotherFunction`
 end
 
 our_object = InheritsFromSomeFunction.new
@@ -26,6 +21,5 @@ puts our_object.another_method
 some_js_object = `new SomeFunction()`
 puts some_js_object.some_method
 
-another_js_object = `new AnotherFunction()`
 puts some_js_object.class
-puts another_js_object.class
+puts our_object.class
